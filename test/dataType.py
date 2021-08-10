2e529976ad9b3540b7037d9db96567a063b312d1
zhangting <zhangting@ainnnovation>
2021-08-10 19:46:59 +0800

    feat:数据类型/关键字/格式缩紧/注释

---
+++ test/dataType.py
@@ -0,0 +1,21 @@
+# 数据类型
+# 数字有四种类型：整数、布尔型、浮点数、复数
+# 整数： int
+# bool
+# float
+# complex(复数): 1+2j、 1.1+2.2j
+# 字符串String
+# 单引号和双引号使用完全相同，使用三引号''' 或 """来指定一个多行字符串
+# 输出---------
+# word 字符串
+# sentence 这是一个句子。
+# paragraph 这是一个段落，
+# 可以由多行组成
+if __name__== "__main__":
+  word = '字符串'
+  sentence="这是一个句子。"
+  paragraph= """这是一个段落，
+  可以由多行组成"""
+  print("word",word)
+  print("sentence",sentence)
+  print("paragraph",paragraph)
---
+++ test/suojin.py
@@ -0,0 +1,5 @@
+if __name__== "__main__":
+   if True:
+       print("True")
+   else:
+       print("False")
