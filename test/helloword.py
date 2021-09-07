7afeec355c2a8f06b3710acf50fc61b8267b21d6
zhangting <1510079209@qq.com>
2021-09-07 21:57:18 +0800

    feat:python输出不换行

---
+++ test/helloword.py
@@ -0,0 +1,19 @@
+print("Hello ,World")
+
+print("你好，世界")
+
+# python默认输出是换行的，不需要换行的话需要在变量末尾加上逗号
+print("I'm a big big girl");print("in a big big world")
+
+
+# python2不换行输出 使用逗号
+print("-------不换行输出--------")
+print("I'm a big big girl"),
+print("in a big big world"),
+
+x = "a"
+y = "b"
+
+# ab python3不换行输出，使用end=""
+print(x,end="")
+print(y,end="")
--- test/keyword.py
+++ test/keyword.py
@@ -1,9 +1,10 @@
-import keyword
+# import keyword
 
 
 # error:ImportError: cannot import name 'iskeyword' from 'keyword' (/Users/admin/Downloads/company-project/Project/personal_project/01my-python-project/test/keyword.py)
 if __name__== "__main__":
-    keyword.kwlist
-    print('key:',"hello")
+    pass
+    # keyword.kwlist
+    # print('key:',"hello")
 
 
