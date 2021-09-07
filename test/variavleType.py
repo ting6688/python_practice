26e588556e165339b3b49aff099f852b7cfa85cb
zhangting <1510079209@qq.com>
2021-09-07 22:04:45 +0800

    feat:python变量赋值

---
+++ test/variavleType.py
@@ -0,0 +1,23 @@
+# 变量类型
+# python中变量赋值不需要类型声明
+# 每个变量在使用前都必须赋值，变量赋值以后该变量才会被创建
+counter = 100;
+miles = 1000.0
+name = "John"
+
+print(counter)
+print(miles)
+print(name)
+
+# a: 1 多个变量赋值
+a=b=c=1
+print("a:",a)
+print("b:",b)
+print("c:",c)
+
+a,b,c=1,2,"John"
+print("a:",a)
+print("b:",b)
+print("c:",c)
+
+
